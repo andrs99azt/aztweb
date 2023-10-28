825bb539e999ea5cf780385e14e669386bcce7cb
Luis Andres Rodriguez Campos <luis.rodriguez@AZTECA.LOC>
2023-10-27 18:36:52 -0600

    Se agrego la estruruta de enrutatamiento, intalacion de dependencias y definicion de estructura pricipal

---
+++ src/pages/Router.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Inicio from './inicio';
+import Nosotros from './nosotros';
+import Servicios from './servicios';
+import CapitalHumano from './capita-humano';
+import Contacto from './contacto';
+
+
+const AppRouter = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Inicio />} />
+                <Route path="/nosotros" element={<Nosotros />} />
+                <Route path="/servicios" element={<Servicios />} />
+                <Route path="/capital-humano" element={<CapitalHumano />} />
+                <Route path="/contacto" element={<Contacto />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default AppRouter;
---
+++ src/pages/servicios/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Index = () => {
+    const navigate = useNavigate();
+
+    return (
+        <div>
+            <h1>Pagina de servicios</h1>
+            <button onClick={() => navigate('/')}>Ir al inicio</button>
+            <button onClick={() => navigate('/nosotros')}>Ir al nosotros</button>
+            <button onClick={() => navigate('/servicios')}>Ir al servicios</button>
+            <button onClick={() => navigate('/capital-humano')}>Ir al capital humano</button>
+            <button onClick={() => navigate('/contacto')}>Ir al contacto</button>
+        </div>
+    );
+}
+
+export default Index;
