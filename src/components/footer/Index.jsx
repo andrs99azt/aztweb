9c1273f9ec2447091914236ee30e6a9673063370
Braulio Miramontes <braulio.miramontes@tlazteca.com.mx>
2023-11-15 12:56:50 -0600

    Se hace la maqueta de la sección contacto

--- src/components/footer/Index.jsx
+++ src/components/footer/Index.jsx
@@ -12,40 +12,41 @@ const Footer = () => {
     return (
         <div>
             <footer>
+                <NavLink to="/inicio" activeClassName="active" className='circle-top d-xl-none d-lg-none d-md-block'></NavLink>
                 <div className="container">
                     <div className="row">
-                        <div className="col-xl-4">
+                        <div className="col-xl-4 col-lg-5 col-md-12">
                             <div className="gris">
                                 <div className="barra-superior"></div>
                                 <img src={logoAzteca} alt="Azteca" className='img-fluid logo'/>
                                 <p className='roboto'>En Transportes Azteca estamos comprometidos en ofrecer un servicio de transporte confiable, seguro y de calidad para satisfacer las necesidades de nuestros clientes</p>
                                 <div className="row icon">
-                                    <div className="col-xl-3">
+                                    <div className="col-xl-3 col-md-3 col-sm-3">
                                         <img src={icono1} alt="Dirección" className='img-fluid'/>
                                     </div>
-                                    <div className="col-xl-9">
+                                    <div className="col-xl-9 col-md-9 col-sm-9">
                                         <h3 className='roboto_bold'>CORPORATIVO <br/>Francisco Montejano Palacios 25-A Col. La Venta del Astillero Zapopan, Jalisco, Mexico. Cp. 45221</h3>
                                     </div>
                                 </div>
                                 <div className="row icon">
-                                    <div className="col-xl-3">
+                                    <div className="col-xl-3 col-md-3 col-sm-3">
                                         <img src={icono2} alt="Teléfono" className='img-fluid'/>
                                     </div>
-                                    <div className="col-xl-9">
+                                    <div className="col-xl-9 col-md-9 col-sm-9">
                                         <h3 className='roboto_bold'>Tel. (33) 3151-0720</h3>
                                     </div>
                                 </div>
                                 <div className="row icon">
-                                    <div className="col-xl-3">
+                                    <div className="col-xl-3 col-md-3 col-sm-3">
                                         <img src={icono3} alt="Correo" className='img-fluid'/>
                                     </div>
-                                    <div className="col-xl-9">
+                                    <div className="col-xl-9 col-md-9 col-sm-9">
                                         <h3 className='roboto_bold'>ventas@azteca.com.mx<br/>omaly.barron@tlazteca.com</h3>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div className="col-xl-3 offset-xl-2">
+                        <div className="col-xl-3 offset-xl-2 col-lg-3 offset-lg-1">
                             <div className="links">
                                 <h2 className='roboto_bold'>Links Rapidos</h2>
                                 <ul>
@@ -57,8 +58,8 @@ const Footer = () => {
                                 </ul>
                             </div>
                         </div>
-                        <div className="col-xl-2 offset-xl-1 position-relative">
-                            <NavLink to="/inicio" activeClassName="active" className='circle-top'></NavLink>
+                        <div className="col-xl-2 offset-xl-1 col-lg-3 col-md-12 position-relative">
+                            <NavLink to="/inicio" activeClassName="active" className='circle-top d-xl-block d-lg-block d-md-none d-xs-none d-none'></NavLink>
                             <ul className='social'>
                                 <li>
                                     <NavLink to="https://facebook.com">
--- src/pages/contacto/index.jsx
+++ src/pages/contacto/index.jsx
@@ -1,12 +1,88 @@
 import React from 'react';
 import Header from '../../components/header/Index';
+import Footer from '../../components/footer/Index';
+import logoAzteca from '../../assets/icons/logo-azteca.png';
 
+//Styles
+import './contacto.scss';
 
 const Index = () => {
 
     return (
         <div>
             <Header/>
+            <div className="container-fluid azteca-cabecera-contacto">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-xl-6">
+                            <h1 className='raleway_bold'>Ponte en contacto <span>con nosotros</span></h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="container">
+                <div className="row azteca-formulario-contacto">
+
+                    <div className="col-xl-6">
+
+                        <div className="informacion">
+                            <p className='raleway_light'>Cuéntanos de que forma podemos ayudar a crecer tu negocio</p>
+                            <ul>
+                                <li>Francisco Montejano Palacios 25-A</li>
+                                <li>La Venta del Astillero, Jalisco, C.P 45221.</li>
+                                <li>Zapopan, México.</li>
+                                <li>(33) 3151-0720</li>
+                                <li>ventas@tlazteca.com.mx</li>
+                            </ul>
+                            <img src={logoAzteca} alt="Transportes Azteca" className='img-luid logo'/>
+                        </div>
+
+                    </div>
+                    <div className="col-xl-6">
+
+                        <form action="#">
+                            <div className="row">
+                                <div className="col-xl-6 col-lg-6 col-md-6">
+                                    <div className="form-group">
+                                        <input type="text" className='form-control' placeholder='Nombres'/>
+                                    </div>
+                                </div>
+                                <div className="col-xl-6 col-lg-6 col-md-6">
+                                    <div className="form-group">
+                                        <input type="text" className='form-control' placeholder='Apellidos'/>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="form-group">
+                                <input type="email" className='form-control' placeholder='Email'/>
+                            </div>
+
+                            <div className="row">
+                                <div className="col-xl-6 col-lg-6 col-md-6">
+                                    <div className="form-group">
+                                        <input type="text" className='form-control' placeholder='Empresa'/>
+                                    </div>
+                                </div>
+                                <div className="col-xl-6 col-lg-6 col-md-6">
+                                    <div className="form-group">
+                                        <input type="tel" className='form-control' placeholder='N° de teléfono'/>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="form-group">
+                                <textarea name="mensaje" className="form-control" placeholder='Mensaje' cols="30" rows="3"></textarea>
+                            </div>
+
+                            <button type="submit" className='btn btn-contacto'>Enviar mensaje</button>
+
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+            <Footer/>
         </div>
     );
 }
